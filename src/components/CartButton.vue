bd6f78139403fea7789f71d489e53341bdb5f58a
Andrej <andrejzabojnik1@gmail.com>
2024-01-14 17:00:40 +0100

    first commit

---
+++ src/components/CartButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    v-if="addedToCart.length > 0"
+    class="fixed bottom-4 flex px-6 items-center justify-center text-white h-10 w-auto whitespace-normal bg-primary rounded-lg right-4 rounded-bl-none"
+  >
+    Máte {{ addedToCart.length }} produkty v košíku
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  components: {},
+
+  props: {
+    addedToCart: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
